644879b638d06fbe9bd819494f28c5732939d5a5
MrShifu01 <stander.christian.com>
2023-08-09 12:24:49 +0200

    mobile version looking good

--- js/script.js
+++ js/script.js
@@ -2,18 +2,21 @@ const container = document.querySelector(".container");
 const sections = gsap.utils.toArray(".container section");
 const mask = document.querySelector(".mask");
 
-let scrollTween = gsap.to(sections, {
-  xPercent: -100 * (sections.length - 1),
-  ease: "none",
-  scrollTrigger: {
-    trigger: ".container",
-    pin: true,
-    scrub: 1,
-    end: "+=20000",
-    onUpdate: self => {
-      // Update the mask width based on the progress of the carousel's ScrollTrigger
-      gsap.set(mask, { width: self.progress * 65 + "%" });
+if (window.innerWidth > 960) {
+  // Horizontal Scrolling for Larger Screens
+  const scrollTween = gsap.to(sections, {
+    xPercent: -100 * (sections.length - 1),
+    ease: "none",
+    scrollTrigger: {
+      trigger: ".container",
+      pin: true,
+      scrub: 1,
+      end: "+=20000",
+      onUpdate: self => {
+        gsap.set(mask, { width: self.progress * 65 + "%" });
+      }
     }
-  }
-});
+  });
+}
+
 
