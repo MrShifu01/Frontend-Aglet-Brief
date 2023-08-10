de5c1054afe6accc64a2e73a15b5f2990212becf
MrShifu01 <stander.christian.com>
2023-08-10 07:59:19 +0200

    contact page done, now just a few tweaks to the responsive design

--- js/script.js
+++ js/script.js
@@ -19,4 +19,51 @@ if (window.innerWidth > 960) {
   });
 }
 
+const dimSections = document.querySelectorAll(".image-container");
+
+dimSections.forEach(section => {
+    section.addEventListener("mouseover", function() {
+        // Add the 'dimmed' class to all dimSections
+        dimSections.forEach(sec => sec.classList.add("dimmed"));
+        
+        // Remove the 'dimmed' class from the currently hovered section
+        this.classList.remove("dimmed");
+    });
+
+    section.addEventListener("mouseout", function() {
+        // Remove the 'dimmed' class from all dimSections
+        dimSections.forEach(sec => sec.classList.remove("dimmed"));
+    });
+});
+
+const overlay = document.getElementById('transition-overlay');
+const loadingText = document.getElementById('loading-text');
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(link => {
+    link.addEventListener('click', function(event) {
+        event.preventDefault(); // stop the default link action
+
+        // Bring in the overlay from the left
+        overlay.style.transform = 'translateX(0)';
+
+        // Show loading animation after the overlay transition is completed
+        setTimeout(() => {
+            loadingText.style.display = 'block';
+
+            // After displaying the loading text for a while, navigate to the actual link
+            setTimeout(() => {
+                window.location.href = link.getAttribute('href');
+            }, 2500); // 2.5 seconds for displaying the loading text (change as per your requirement)
+        }, 100); // delay equal to the overlay transition time
+    });
+});
+
+// Body fade in
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.classList.add('fade-in');
+});
+
+
+
 
