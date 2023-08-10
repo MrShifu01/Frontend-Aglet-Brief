d0127617b121e162915c583d0c9d5f3378937f21
MrShifu01 <stander.christian.com>
2023-08-10 08:54:07 +0200

    Seems mostly done

--- js/playvid.js
+++ js/playvid.js
@@ -1,16 +1,18 @@
 // Select all image containers
 const containers = document.querySelectorAll('.image-container');
 
-containers.forEach(container => {
-    // When the mouse enters the container, play the video
-    container.addEventListener('mouseover', function() {
-        const video = container.querySelector('video');
-        if (video) video.play();
+if (window.innerWidth > 960) {
+    containers.forEach(container => {
+        // When the mouse enters the container, play the video
+        container.addEventListener('mouseover', function() {
+            const video = container.querySelector('video');
+            if (video) video.play();
+        });
+    
+        // When the mouse leaves the container, pause the video
+        container.addEventListener('mouseout', function() {
+            const video = container.querySelector('video');
+            if (video) video.pause();
+        });
     });
-
-    // When the mouse leaves the container, pause the video
-    container.addEventListener('mouseout', function() {
-        const video = container.querySelector('video');
-        if (video) video.pause();
-    });
-});
+}
